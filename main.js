b145ef17eb28187865fa61d06b6b483bb45f474c
GiovaniJuninhoo <130391012+GiovaniJuninhoo@users.noreply.github.com>
2023-09-26 15:38:32 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+document.querySelector('.tecla_pom');
+function tocaSompom(){
+
+document.querySelector('#som_tecla_pom').play('');
+}
+document.querySelectorAll('.tecla')
+
+const ListaDeTeclas = document.querySelectorAll('.tecla');
+document.querySelector('.teclapom').onclickc = tocaSompom ();
